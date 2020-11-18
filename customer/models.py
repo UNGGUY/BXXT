2658599a78b7db5fdd65edcb778193fcf1726d0f
王峻彪 <52520965+wang4896077@users.noreply.github.com>
2020-11-18 20:46:02 +0800

    models.py finish

--- customer/models.py
+++ customer/models.py
@@ -1,27 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+# import MySQLdb
 
 
 # Create your models here.
 
 class Manager(models.Model):
     """超级用户表/管理员表"""
-    choice = (
-        ('auditer', '审核人'),
-        ('manager', '审核管理人'),
-        ('admin', '后台管理员'),
-        ('checker', '收单员'),
-    )
+    class Type(models.TextChoices):
+        admin = 0, _('管理员')
+        manager = 1, _('审核负责人')
+        auditer = 2, _('审核人')
+        checker = 3, _('收单员')
+
     mid = models.CharField(max_length=20)  # 账户Mid
     pw = models.CharField(max_length=16)  # 账户密码pw
     mname = models.CharField(max_length=20)  # 用户姓名mname
-    type = models.CharField('身份', max_length=20, choices=choice)  # 身份type
-    isDelete = models.NullBooleanField(default=False)  # 删除标识 默认default  或 true null
+    type = models.SmallIntegerField('身份', choices=Type.choices)
+    isDelete = models.BooleanField(default=False, null=True)  # 删除标识 默认default  或 true null
 
     def __str__(self):
         return self.mid
 
     class Meta:
         db_table = "manager"
+        verbose_name = '员工'
+        verbose_name_plural = verbose_name
 
 
 class UserType(models.Model):
@@ -34,12 +38,14 @@ class UserType(models.Model):
 
     class Meta:
         db_table = "usertype"
+        verbose_name = '人员类型'
+        verbose_name_plural = verbose_name
 
 
 class User(models.Model):
     gender = (
-        ('male', '男'),
-        ('female', '女'),
+        (1, '男'),
+        (0, '女'),
     )
     ######################
     # 注意，这里的用户名是网络上的用户名
@@ -54,7 +60,7 @@ class User(models.Model):
     # 身份
     utype = models.ForeignKey(UserType, on_delete=models.PROTECT)
     # 性别
-    sex = models.CharField('性别', max_length=10, choices=gender, default='男')
+    sex = models.BinaryField('性别', choices=gender)
     # 报销金额
     money = models.DecimalField('报销金额', max_digits=8, decimal_places=2)
     # 年龄
@@ -62,33 +68,33 @@ class User(models.Model):
     # 省份
     address = models.CharField('省份', max_length=20, default="北京")
 
-    isDelete = models.NullBooleanField('三元态', default=False)
+    isDelete = models.BooleanField('删除状态', default=False, null=True)
 
     def __str__(self):
         return self.uname
 
     class Meta:
         verbose_name = '用户'
-        verbose_name_plural = '用户'
+        verbose_name_plural = verbose_name
         db_table = "user"
 
 
 # 申请记录表
 class Apply(models.Model):
     choice = (
-        ('uncommitted', '未提交'),
-        ('applying', '申请中'),
-        ('verify', '待确认'),
-        ('waiting', '待报销'),
-        ('perfect', '已报销'),
+        (0, '未提交'),
+        (1, '申请中'),
+        (2, '待确认'),
+        (3, '待报销'),
+        (4, '已报销'),
     )
     # 编号
     aid = models.CharField('编号', max_length=18)
     # 发起人id
     uid = models.ForeignKey(User, on_delete=models.PROTECT)
     # 申请的状态
-    astatus = models.CharField("申请状态", max_length=20, choices=choice, default='未提交')
-    isDelete = models.NullBooleanField(default=False)  # 删除标识 默认default  或 true null
+    astatus = models.SmallIntegerField("申请状态", choices=choice, default='未提交')
+    isDelete = models.BooleanField(default=False, null=True)  # 删除标识 默认default  或 true null
 
     def __str__(self):
         return self.aid
@@ -96,7 +102,7 @@ class Apply(models.Model):
     class Meta:
         db_table = "apply"
         verbose_name = '申请记录'
-        verbose_name_plural = '申请记录'
+        verbose_name_plural = verbose_name
 
 
 class Record(models.Model):
@@ -112,19 +118,23 @@ class Record(models.Model):
 
     class Meta:
         db_table = "record"
+        verbose_name = '报销记录'
+        verbose_name_plural = verbose_name
 
 
 class Section(models.Model):
     stype = models.CharField(max_length=20)  # 科室类型
     sid = models.CharField(max_length=20)  # 科室编号
     # 删除标记，true 删除 false 未删除 null
-    isDelete = models.NullBooleanField()
+    isDelete = models.BooleanField(null=True)
 
     def __str__(self):
         return self.stype
 
     class Meta:
         db_table = "section"
+        verbose_name = '科室'
+        verbose_name_plural = verbose_name
 
 
 class Detail(models.Model):
@@ -134,21 +144,21 @@ class Detail(models.Model):
     did = models.CharField(max_length=10)  # 编号did
     dtime = models.DateField(auto_now=False, auto_now_add=False)  # 时间
     choice=(
-        ('change', '转诊单'),
-        ('register', '挂号费'),
-        ('cost', '发票'),
-        ('extra', '补充'),
+        (0, '转诊单'),
+        (1, '挂号费'),
+        (2, '发票'),
+        (3, '补充'),
     )
-    type = models.CharField('类型', max_length=20,choices=choice)
+    type = models.SmallIntegerField('类型', choices=choice)
     money = models.DecimalField(max_digits=8, decimal_places=2, null=True)  # 金额money 6位.2位
     hname = models.CharField(max_length=20, null=True)  # 医院hname
     sid = models.ForeignKey(Section, on_delete=models.PROTECT, null=True)  # 科室sid
     status = (
-        ('verify', '验证'),
-        ('success', '合格'),
-        ('fail', '不合格')
+        (0, '验证'),
+        (1, '合格'),
+        (-1, '不合格')
     )
-    dstatus = models.CharField('状态', max_length=20, choices=status, default='验证')  # 表单状态 默认false是合格
+    dstatus = models.SmallIntegerField('状态', choices=status, default='验证')  # 表单状态 默认false是合格
     folder = models.ImageField()
 
     def __str__(self):
@@ -156,6 +166,8 @@ class Detail(models.Model):
 
     class Meta:
         db_table = "detail"
+        verbose_name = '凭证'
+        verbose_name_plural = verbose_name
 
 
 class Hospital(models.Model):
@@ -164,13 +176,15 @@ class Hospital(models.Model):
     #  医院名称
     hname = models.CharField(max_length=100)
     #  删除标记，true 删除 false 未删除 null
-    isDelete = models.NullBooleanField()
+    isDelete = models.BooleanField(null=True)
 
     def __str__(self):
         return self.hname
 
     class Meta:
         db_table = "hospital"
+        verbose_name = '医院'
+        verbose_name_plural = verbose_name
 
 
 class Ratio(models.Model):
@@ -180,16 +194,18 @@ class Ratio(models.Model):
 
     class Meta:
         db_table = "ratio"
+        verbose_name = '报销比例'
+        verbose_name_plural = verbose_name
 
 
 class Audit(models.Model):
     choice = (
-        ('finish', '完成'),
-        ('undo', '撤销'),
+        (1, '完成'),
+        (0, '撤销'),
     )
     aid = models.ForeignKey(Apply, on_delete=models.CASCADE)
     auid = models.CharField(max_length=10)
-    austatus = models.CharField('状态', max_length=10, choices=choice, default='完成')
+    austatus = models.SmallIntegerField('状态', choices=choice, default='完成')
     mid = models.ForeignKey(Manager, on_delete=models.PROTECT)
     autime = models.DateTimeField(auto_now_add=True)
 
@@ -198,6 +214,8 @@ class Audit(models.Model):
 
     class Meta:
         db_table = "audit"
+        verbose_name = '审核记录'
+        verbose_name_plural = verbose_name
 
 
 
