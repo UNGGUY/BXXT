21aaa8d3ebadd5ad95684fed21f1b2f3e1cff407
UNGGUY <734916262@qq.com>
2020-11-29 16:52:05 +0800

    add applys.html records.html

--- customer/urls.py
+++ customer/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('login/',views.login,name='login'),
     path('account/',views.account,name='account'),
     path('business/',views.business,name='business'),
-    path('search/',views.search,name='search'),
+    path('applys/',views.applys,name='applys'),
+    path('records/<int:apply_id>/',views.records,name='records'),
+    path('details/<int:record_id>/',views.details,name='details'),
+    path('detail/<int:detail_id>/',views.detail,name='detail'),
 ]
