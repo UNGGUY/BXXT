f247018de51ab4f66c3a6f62d2ae95fe04fb7790
王峻彪 <52520965+wang4896077@users.noreply.github.com>
2020-12-08 02:12:39 +0800

    customer fixed

--- customer/models.py
+++ customer/models.py
@@ -84,7 +84,7 @@ class User(models.Model):
     # 手机号
     tel = models.CharField('手机号', max_length=11)
     # 身份
-    utype = models.ForeignKey(UserType, on_delete=models.PROTECT, verbose_name=u"人员类型", default=1)
+    utype = models.ForeignKey(UserType, on_delete=models.PROTECT, verbose_name=u"人员类型", default='1')
     # 性别
     sex = models.CharField('性别', max_length=20, choices=gender)
     # 报销金额
